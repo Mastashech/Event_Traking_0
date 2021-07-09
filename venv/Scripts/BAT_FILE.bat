70a18296757c169ee33c1ee2e6c5aabd78fe7d16
Mastashech <Mastashech@yandex.ru>
2021-07-09 18:39:55 +0300

    Initial commit

---
+++ venv/Scripts/BAT_FILE.bat
@@ -0,0 +1,8 @@
+pyuic5 -x -o C:\Users\maxgu\Event Traking\UIfiles\входИЛИрегистрация.ui -o C:
+\Users\maxgu\Event Traking\UIfiles\входИЛИрегистрация.py
+
+pyuic5 -x -o C:\Users\maxgu\Event Traking\UIfiles\НастройкаСобытия.ui -o C:
+\Users\maxgu\Event Traking\UIfiles\НастройкаСобытия.py
+
+pyuic5 -x -o C:\Users\maxgu\Event Traking\UIfiles\Событие.ui -o C:
+\Users\maxgu\Event Traking\UIfiles\Событие.py
